7c05166a2285057f019760acfed8af9943f341a0
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2025-07-01 11:44:31 +0200

    Update Affiches

--- src/Controller/Admin/AfficheCrudController.php
+++ src/Controller/Admin/AfficheCrudController.php
@@ -7,6 +7,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 
 class AfficheCrudController extends AbstractCrudController
 {
@@ -15,14 +17,19 @@ class AfficheCrudController extends AbstractCrudController
         return Affiche::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            IdField::new('id')->hideOnForm(),
+            ImageField::new('image')
+                ->setBasePath('uploads/affiches')
+                ->setUploadDir('public/uploads/affiches')
+                ->setUploadedFileNamePattern('[slug]-[timestamp].[extension]')
+                ->setRequired(true),
+            TextField::new('titre'),
+            TextField::new('description'),
+            DateTimeField::new('date'),
+            TextField::new('lieu'),
         ];
     }
-    */
 }
--- src/Controller/AgendaController.php
+++ src/Controller/AgendaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AfficheRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,15 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class AgendaController extends AbstractController
 {
     #[Route('/agenda/affiches', name: 'agenda_affiches')]
-    public function affiches(): Response
+    public function affiches(AfficheRepository $afficheRepository): Response
     {
-        return $this->render('agenda/affiches.html.twig');
+        $affiches = $afficheRepository->findBy([], ['date' => 'ASC']);
+
+        return $this->render('agenda/affiches.html.twig', [
+            'affiches' => $affiches,
+        ]);
     }
 
     #[Route('/agenda/calendrier', name: 'agenda_calendrier')]
     public function calendrier(): Response
     {
         return $this->render('agenda/calendrier.html.twig');
-
     }
 }
